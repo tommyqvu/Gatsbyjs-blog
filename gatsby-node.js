9803e6e331eec6929805d146158bf5a94529647b
Thomas Vu <thomasvu@Thomass-MacBook-Air.local>
2019-08-10 23:26:06 +0200

    initial commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,36 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const { createFilePath } = require(`gatsby-source-filesystem`)
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(res=>{
+    result.data.allMarkdownRemark.edges.forEach(({node})=>{
+      createPage({
+        path: node.fields.slug,
+        component:
+      })
+    })
+  })
+}
